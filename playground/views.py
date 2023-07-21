9fac75f96d084237d974e4f01ba8571c1dfd1052
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-21 20:04:54 +0300

    5.5 get done some examples of retrievring objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
     
-    query_set = Product.objects.all().count()
-    for product in query_set:
-        print(product)
-
+    #1: exists = Product.objects.filter(pk=0).exists()
+    
+    #2 try:
+    #     product = Product.objects.get(pk=0)
+    # except:
+    #     pass
+    
+    #3 product = Product.objects.filter(pk=0).first()
+    
     return render(request,'hello.html',{'name':'erfan'})
