d6337d491aae241886348d8c4c74d662a04c7b23
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-25 20:17:32 +0300

    5.6 part_2 order_by() desc& html 2 attr same line.

--- playground/views.py
+++ playground/views.py
@@ -6,6 +6,6 @@ from store.models import Product
 
 def say_hello(request):
     # to order in 'x' prameter quote in prenteces like : order_by('x')
-    queryset = Product.objects.all().order_by('unit_price')
+    queryset = Product.objects.all().order_by('unit_price')[::-1]
 
     return render(request,'hello.html',{'name':'erfan','products': list(queryset)})
