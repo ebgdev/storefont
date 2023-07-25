fb40190c9375647aface1783bba954ccd489937d
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-25 20:08:58 +0300

    5.6 part1

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # to order in 'x' prameter quote in prenteces like : order_by('x')
+    queryset = Product.objects.all().order_by('unit_price')
+
+    return render(request,'hello.html',{'name':'erfan','products': list(queryset)})
