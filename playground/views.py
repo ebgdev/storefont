a9366151a354e8e7f6537a440645cd36c413f2e4
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-26 19:32:51 +0300

    5.7 Q objects get done.

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    # to order in 'x' prameter quote in prenteces like : order_by('x')
-    queryset = Product.objects.all().order_by('unit_price')[::-1]
-
+    queryset = Product.objects.filter(Q(inventory__lt = 10) & ~Q(unit_price__lt=20))
     return render(request,'hello.html',{'name':'erfan','products': list(queryset)})
