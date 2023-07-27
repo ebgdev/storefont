d3f5a6f436cdc04d91ef960adc9d6957609b63db
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-27 16:37:35 +0300

    5.8 - F objects done

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q,F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(Q(inventory__lt = 10) & ~Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('collection__id'))
     return render(request,'hello.html',{'name':'erfan','products': list(queryset)})
