7fc419be362f5be6923eac32cd81fd57ed58a5de
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-03 18:25:03 +0300

    5.11.2 my approach

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q,F
+from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
-
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    queryset = Product.objects.values('id','title','collection__title').order_by('id')
-    return render(request,'hello.html',{'name':'erfan','products': queryset})
+    queryset = OrderItem.objects.values('product__title').distinct().order_by('product__title')
+    # queryset_titles = queryset.keys()
+    result_count = queryset.count()
+    return render(request, 'hello.html', {'name': 'erfan', 'products': queryset, 'result_count': result_count})
