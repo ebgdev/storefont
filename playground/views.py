c6188a9023a489a1f67d238ebe2083eff574b6aa
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-08 00:33:03 +0300

    5.13 this is course way.

--- playground/views.py
+++ playground/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, Order , OrderItem
 
 def say_hello(request):
-    last_order = OrderItem.objects.values_list('order__customer__first_name','product__title','order__placed_at').order_by('-order__placed_at')[:7]
-    result_count = last_order.count()
-    return render(request, 'hello.html', {'name': 'erfan', 'orders': last_order, 'result_count': result_count})
+    queryset = Order.objects.select_related('customer').order_by('-placed_at')[:5]
+    result_count = queryset.count()
+    return render(request, 'hello.html', {'name': 'erfan', 'orders': list(queryset), 'result_count': result_count})
