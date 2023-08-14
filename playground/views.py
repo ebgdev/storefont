6f573214dc5bd64bf66d476fc28d1ad66e3f2943
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-15 00:20:57 +0300

    5.14 'aggregate' get done

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models.aggregates import *
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Order , OrderItem
 
 def say_hello(request):
-    queryset = Order.objects.select_related(
-        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    result_count = queryset.count()
-    return render(request, 'hello.html', {'name': 'erfan', 'orders': list(queryset), 'result_count': result_count})
+    result = Product.objects.aggregate(we_can_say = Count('id'),min_price = Min('unit_price'))
+    return render(request, 'hello.html', {'name': 'erfan', 'result': result})
