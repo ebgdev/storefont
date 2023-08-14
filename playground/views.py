032562c7eaeb704f538991a28e92d936e8269a5a
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-14 23:53:28 +0300

    5.13 prefetch reverse relation - question finished

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Order , OrderItem
 
 def say_hello(request):
-    queryset = Order.objects.select_related('customer').order_by('-placed_at')[:5]
+    queryset = Order.objects.select_related(
+        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
     result_count = queryset.count()
     return render(request, 'hello.html', {'name': 'erfan', 'orders': list(queryset), 'result_count': result_count})
