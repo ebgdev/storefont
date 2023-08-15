5a22409e1c80f414e77101a3861343a0516850f1
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-15 15:46:03 +0300

    5.16.1 calling db funcs

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import *
-from django.db.models import Q, F , Value
+from django.db.models import Q, F , Value , Func
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Order , OrderItem , Customer
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(new_id = F('id') + 1)
-    return render(request, 'hello.html', {'name': 'erfan', 'queryset': list(queryset)})
+    queryset = Customer.objects.annotate(
+        #CONCAT
+        full_name = Func(F(('first_name')),Value(' '),F('last_name'),function = 'CONCAT')
+    )
+    result_count = queryset.count()
+    return render(request, 'hello.html', {'name': 'erfan','result_count':result_count ,'queryset': list(queryset)})
