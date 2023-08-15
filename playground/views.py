6124dfda9c0fa8574458dadddd94bac1d2438e53
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-15 14:24:30 +0300

    5.15 annotating objects is done

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import *
-from django.db.models import Q, F
+from django.db.models import Q, F , Value
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, Order , OrderItem
+from store.models import Product, Order , OrderItem , Customer
 
 def say_hello(request):
-    result = Product.objects.aggregate(we_can_say = Count('id'),min_price = Min('unit_price'))
-    return render(request, 'hello.html', {'name': 'erfan', 'result': result})
+    queryset = Customer.objects.annotate(new_id = F('id') + 1)
+    return render(request, 'hello.html', {'name': 'erfan', 'queryset': list(queryset)})
