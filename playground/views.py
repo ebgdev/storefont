e1d3059fd18e3544a83db246fb31dd1d533ad8bc
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-29 20:21:29 +0300

    5.19 get done - Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.db.models.aggregates import *
-from django.db.models import Q, F , Value , Func , ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
+# from django.http import HttpResponse
+# from django.db.models.aggregates import *
+# from django.db.models import Q, F , Value , Func , ExpressionWrapper, DecimalField
+# from django.db.models.functions import Concat
+# from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Order , OrderItem , Customer
 
 def say_hello(request):
-    # for monitoring values we should always use DeciamlField
-    discounted_price =  ExpressionWrapper(F('unit_price') * 0.8 ,output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price =  discounted_price
-    )
-    result_count = queryset.count()
-    return render(request, 'hello.html', {'name': 'erfan','result_count':result_count ,'queryset': list(queryset)})
+    content_type = ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type = content_type,
+            object_id = 1,
+        )
+    return render(request, 'hello.html', {'name': 'erfan','tags': list(queryset)})  
